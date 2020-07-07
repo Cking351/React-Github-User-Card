6a50cb084ccd787f535f304ea52091663cdbac45
Chris King <cking5836@gmail.com>
2020-07-07 18:55:27 -0400

    Added reactstrap and styled

--- github-react-card/src/Components/GitCard.js
+++ github-react-card/src/Components/GitCard.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import axios from 'axios';
-
+import './card.css'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
+  
 function GitCard(props) {
     return (
-        <div>
-            <img src={props.data.avatar_url} />
-            <h4>{props.data.name}</h4>
-            <h4>{props.data.location}</h4>
-            <p>{props.data.bio}</p>
-        </div>
+        <div className='container'>
+      <Card>
+        <CardImg top width="30%" src={props.data.avatar_url}/>
+        <CardBody>
+        <CardTitle>{props.data.name}</CardTitle>
+          <CardSubtitle>{props.data.location}</CardSubtitle>
+          <CardText>{props.data.bio}</CardText>
+          <a href="https://github.com/Cking351"><Button>Click here to see this repo</Button></a>
+        </CardBody>
+      </Card>
+    </div>
     )
 }
 
